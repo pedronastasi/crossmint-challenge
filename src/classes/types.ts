bf15e1df4e55da3b441d289be71b3c5b32fbcb03
pedro nastasi <pedronastasi@hotmail.com>
2023-10-16 20:22:51 -0300

    chore(types.ts): add new types

--- src/classes/types.ts
+++ src/classes/types.ts
@@ -1,15 +1,26 @@
-export interface Coordenates {
+import { MegaverseClient } from "./MegaverseClient";
+
+export interface AstralObjectProps {
+  column: number;
+  row: number;
+  type: Content["type"];
+}
+
+export interface PolyanetProps {
   column: number;
   row: number;
 }
 
-export interface GoalMap {
-  goal: GoalElement[][];
+export interface SoloonProps {
+  column: number;
+  row: number;
+  color: Color;
 }
 
-export enum GoalElement {
-  Polyanet = "POLYANET",
-  Space = "SPACE",
+export interface ComethProps {
+  column: number;
+  row: number;
+  direction: Direction;
 }
 
 export interface CandidateMap {
@@ -25,5 +36,76 @@ export interface Map {
 }
 
 export interface Content {
-  type: number;
+  type: 0 | 1 | 2;
+  color?: Color;
+  direction?: Direction;
+}
+
+export type Color = "blue" | "red" | "purple" | "white";
+export type Direction = "up" | "down" | "left" | "right";
+
+export interface GoalMap {
+  goal: GoalElement[][];
+}
+
+export enum GoalElement {
+  BlueSoloon = "BLUE_SOLOON",
+  DownCometh = "DOWN_COMETH",
+  LeftCometh = "LEFT_COMETH",
+  Polyanet = "POLYANET",
+  PurpleSoloon = "PURPLE_SOLOON",
+  RedSoloon = "RED_SOLOON",
+  RightCometh = "RIGHT_COMETH",
+  Space = "SPACE",
+  UpCometh = "UP_COMETH",
+  WhiteSoloon = "WHITE_SOLOON",
+}
+
+export interface AstralObjectStrategy {
+  execute: () => Promise<void>;
+}
+
+export interface AstralObjectFactory {
+  create: () => Promise<void>;
+}
+
+export type GoalMapToCandidateMap = {
+  [key in GoalElement]: {
+    type: 0 | 1 | 2;
+    color?: Color;
+    direction?: Direction;
+  } | null;
+};
+
+export interface CreateAstralObjectsStrategyProps {
+  astralObjectsFactories: AstralObjectFactory[];
+  megaverseClient: MegaverseClient;
+  goal: GoalElement[][];
+}
+
+export type GoalComethsToCandidateMap = Record<
+  | GoalElement.UpCometh
+  | GoalElement.DownCometh
+  | GoalElement.LeftCometh
+  | GoalElement.RightCometh,
+  {
+    type: 2;
+    direction: Direction;
+  }
+>;
+
+export type GoalSoloonsToCandidateMap = Record<
+  | GoalElement.RedSoloon
+  | GoalElement.WhiteSoloon
+  | GoalElement.BlueSoloon
+  | GoalElement.PurpleSoloon,
+  {
+    type: 1;
+    color: Color;
+  }
+>;
+
+export interface AstralObjectFactoryProps {
+  MegaverseClient: MegaverseClient;
+  goal: GoalElement[][];
 }
